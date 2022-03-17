e471f191d5f600beb7033d464373fd37fc89540d
sanglee <pksll325@gmail.com>
2022-03-17 19:24:30 +0900

    bug: fixed minor error

--- hw2p2/model/inception_resnet_v1.py
+++ hw2p2/model/inception_resnet_v1.py
@@ -227,7 +227,7 @@ class InceptionResnetV1(nn.Module):
         self.avgpool_1a = nn.AdaptiveAvgPool2d(1)
         self.dropout = nn.Dropout(dropout_prob)
         self.last_linear = nn.Linear(1792, 512, bias=False)
-        self.last_bn = nn.BatchNorm1d(512, eps=0.001, momentum=0.1, affine=True
+        self.last_bn = nn.BatchNorm1d(512, eps=0.001, momentum=0.1, affine=True)
 
         if self.classify and self.num_classes is not None:
             self.logits = nn.Linear(512, self.num_classes)
