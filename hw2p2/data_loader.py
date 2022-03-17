3b06d4ba70957367075c8b2090e9e55b045b8bb1
sanglee <pksll325@gmail.com>
2022-03-17 14:30:11 +0900

    feat: augmentation added

--- hw2p2/data_loader.py
+++ hw2p2/data_loader.py
@@ -15,10 +15,7 @@ import numpy as np
 from submit.classification import ClassificationTestSet
 from config import *
 
-"""
-Transforms (data augmentation) is quite important for this task.
-Go explore https://pytorch.org/vision/stable/transforms.html for more details
-"""
+
 DATA_DIR = "data"
 
 if ARGS.half == True:
@@ -30,14 +27,30 @@ VAL_DIR = osp.join(DATA_DIR, "classification/classification/dev")
 TEST_DIR = osp.join(DATA_DIR, "classification/classification/test")
 
 def load_dataset(batch_size):
+    mean = (0.5, 0.5, 0.5)
+    std = (0.5, 0.5, 0.5)
+
     if ARGS.aug_type is None:
-        train_transforms = [transforms.ToTensor()]
-    val_transforms = [transforms.ToTensor()]
+        train_transforms = transforms.Compose([
+                            transforms.ToTensor(), 
+                            transforms.Normalize(mean=mean, std=std)
+                            ])
+    if ARGS.aug_type == 'basic':
+        train_transforms = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std)
+        ])
+    val_transforms = transforms.Compose([
+                        transforms.ToTensor(), 
+                        transforms.Normalize(mean=mean, std=std)
+                        ])
 
     train_dataset = torchvision.datasets.ImageFolder(TRAIN_DIR,
-                                                    transform=transforms.Compose(train_transforms))
+                                                    transform=train_transforms)
     val_dataset = torchvision.datasets.ImageFolder(VAL_DIR,
-                                                transform=transforms.Compose(val_transforms))
+                                                transform=val_transforms)
 
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size,
@@ -45,7 +58,7 @@ def load_dataset(batch_size):
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,
                             drop_last=True, num_workers=ARGS.num_workers)
 
-    test_dataset = ClassificationTestSet(TEST_DIR, transforms.Compose(val_transforms))
+    test_dataset = ClassificationTestSet(TEST_DIR, val_transforms)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
                             drop_last=False, num_workers=ARGS.num_workers)
 
