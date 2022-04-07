ede6c0a816b9339113f3cd9ad09b129b4993463e
sanglee <pksll325@gmail.com>
2022-04-07 14:06:35 +0900

    feat: baseline organized

--- hw3p2/train.py
+++ hw3p2/train.py
@@ -48,24 +48,28 @@ def calculate_levenshtein(h, y, lh, ly, decoder, PHONEME_MAP):
     # TODO: call the decoder's decode method and get beam_results and out_len (Read the docs about the decode method's outputs)
     # Input to the decode method will be h and its lengths lh 
     # You need to pass lh for the 'seq_lens' parameter. This is not explicitly mentioned in the git repo of ctcdecode.
+    h = h.permute(1,0,2)
     beam_result, beam_scores, timesteps, out_len = decoder.decode(h, seq_lens=lh)
 
-    batch_size = y.shape[0]# TODO
+    batch_size = beam_scores.shape[0]# TODO
 
     dist = 0
 
     for i in range(batch_size): # Loop through each element in the batch
 
-        h_sliced = beam_result[i,:out_len] # TODO: Get the output as a sequence of numbers from beam_results
+        score_max_idx = torch.argmax(beam_scores[i])
+        h_sliced = beam_result[i][score_max_idx][:out_len[i][score_max_idx]] # TODO: Get the output as a sequence of numbers from beam_results
         # Remember that h is padded to the max sequence length and lh contains lengths of individual sequences
         # Same goes for beam_results and out_lens
         # You do not require the padded portion of beam_results - you need to slice it with out_lens 
         # If it is confusing, print out the shapes of all the variables and try to understand
 
-        h_string = [PHONEME_MAP.index(hh) for hh in h_sliced] # TODO: MAP the sequence of numbers to its corresponding characters with PHONEME_MAP and merge everything as a single string
+        h_string = [str(PHONEME_MAP[hh]) for hh in h_sliced] # TODO: MAP the sequence of numbers to its corresponding characters with PHONEME_MAP and merge everything as a single string
+        h_string = "".join(h_string)
 
-        y_sliced = y[i,:ly] # TODO: Do the same for y - slice off the padding with ly
-        y_string = [PHONEME_MAP.index(yy) for yy in y_sliced] # TODO: MAP the sequence of numbers to its corresponding characters with PHONEME_MAP and merge everything as a single string
+        y_sliced = y[i,:ly[i]] # TODO: Do the same for y - slice off the padding with ly
+        y_string = [str(PHONEME_MAP[yy])for yy in y_sliced] # TODO: MAP the sequence of numbers to its corresponding characters with PHONEME_MAP and merge everything as a single string
+        y_string = "".join(y_string)
         
         dist += Levenshtein.distance(h_string, y_string)
 
@@ -79,113 +83,103 @@ def train(epoch, model, train_loader, optimizer, criterion):
     batch_bar = tqdm(total=len(train_loader), dynamic_ncols=True, leave=False, position=0, desc='Train') 
 
     total_loss = 0
-    total_dist = 0
-    total = 0
 
-    log_interval = 40
-
-    for i, (x, y, lx, ly) in enumerate(tqdm(train_loader)):
+    for i, (x, y, lx, ly) in enumerate(train_loader):
         optimizer.zero_grad()
-        total += len(y)
 
         x = x.to(device)
         outputs, length = model(x, lx)
 
         loss = criterion(outputs, y, length, ly)
         loss.backward()
-        total_loss += loss.item()
+        total_loss += float(loss)
 
         optimizer.step()
         optimizer.zero_grad()
 
-        # tqdm lets you add some details so you can monitor training as you train.
-        batch_bar.set_postfix(
-            loss="{:.04f}".format(float(total_loss / (i + 1))),
-            )
-
-        batch_bar.update() # Update tqdm bar
-        batch_bar.close() # You need this to close the tqdm bar
-
-        train_loss = float(total_loss / total)
         lr_rate = float(optimizer.param_groups[0]['lr'])
 
-        
-    print("Epoch {}/{}: Train Loss {:.04f}, Learning Rate {:.04f}".format(
+        # tqdm lets you add some details so you can monitor training as you train.
+        batch_bar.set_postfix(
+            loss="{:.04f}".format(loss),
+            lr="{:.04f}".format(lr_rate)
+        )
+        batch_bar.update() # Update tqdm bar
+    batch_bar.close() # You need this to close the tqdm bar
+    
+    train_loss = total_loss / len(train_loader)    
+    print("Epoch {}/{}: train loss {:.04f}, learning rate {:.04f}".format(
                                         epoch + 1, ARGS.epochs, train_loss, lr_rate))
         
     return train_loss, lr_rate
 
-def validate(model, val_loader, criterion):
+def validate(epoch, model, val_loader, criterion, decoder):
     model.eval()
-    batch_bar = tqdm(total=len(val_loader), dynamic_ncols=True, position=0, leave=False, desc='Val')
+    batch_bar = tqdm(total=len(val_loader), dynamic_ncols=True, position=0, leave=False, desc='Validation')
 
     total_loss = 0
     total_dist = 0
-    total = 0
 
-    log_interval = 40
-
-    for i, (x, y, lx, ly) in enumerate(tqdm(val_loader)):
-        optimizer.zero_grad()
-        total += len(y)
-
+    for i, (x, y, lx, ly) in enumerate(val_loader):
         x = x.to(device)
-        outputs, length = model(x, lx)
+        
+        with torch.no_grad():
+            outputs, length = model(x, lx)
+        dist = calculate_levenshtein(outputs,y,lx,ly,decoder,PHONEME_MAP=PHONEME_MAP)
+        total_dist += dist
 
         loss = criterion(outputs, y, length, ly)
-        total_loss += loss.item()
+        total_loss += loss
 
         # tqdm lets you add some details so you can monitor training as you train.
         batch_bar.set_postfix(
-            loss="{:.04f}".format(float(total_loss / (i + 1))),
-            )
+            loss="{:.04f}".format(loss),
+            dist="{:.04f}".format(dist)
+        )
 
         batch_bar.update() # Update tqdm bar
-        batch_bar.close() # You need this to close the tqdm bar
+    batch_bar.close() # You need this to close the tqdm bar
 
-        val_loss = float(total_loss / total)
-        lr_rate = float(optimizer.param_groups[0]['lr'])
-
+    val_loss = float(total_loss / len(val_loader))
+    val_dist = float(total_dist / len(val_loader))
         
-    print("Validation: Train Loss {:.04f}, Learning Rate {:.04f}".format(val_loss, lr_rate))
+    print("Epoch {}/{}: validation loss {:.04f}, distance {:.04f}".format(epoch + 1, ARGS.epochs, val_loss, val_dist))
 
-    return val_loss
+    return val_loss, val_dist
 
-def decode(output, seq_sizes, beam_width=60):
-    decoder = CTCBeamDecoder(labels=PHONEME_MAP, blank_id=0, beam_width=beam_width)
-    output = torch.transpose(output, 0, 1) 
-    probs = F.softmax(output, dim=2).data #.cpu()
+def make_predictions(h, lh, decoder, PHONEME_MAP):
+    h = h.permute(1,0,2)
+    beam_results, beam_scores, timesteps, out_lens = decoder.decode(h,seq_lens=lh)
+    batch_size = beam_results.shape[0]
 
-    #output, scores, timesteps, out_seq_len = decoder.decode(probs=probs, seq_lens=torch.IntTensor(seq_sizes))
-    output, scores, timesteps, out_seq_len = decoder.decode(probs=probs, seq_lens=seq_sizes)
+    h_string_list = []
+    for i in range(batch_size): # Loop through each element in the batch
+        argmax_index = torch.argmax(beam_scores[i])
+        h_sliced = beam_results[i][argmax_index][:out_lens[i][argmax_index]]
 
+        h_string = [str(PHONEME_MAP[i]) for i in h_sliced] # TODO: MAP the sequence of numbers to its corresponding characters with PHONEME_MAP and merge everything as a single string
+        h_string = "".join(h_string)
+        h_string_list.append(h_string)
+    
+    return h_string_list
 
-    decoded = []
-    for i in range(output.size(0)):
-        chrs = ""
-        if out_seq_len[i, 0] != 0:
-            chrs = "".join(PHONEME_MAP[o] for o in output[i, 0, :out_seq_len[i, 0]])
-        decoded.append(chrs)
-    return decoded
-
-def test(model, test_loader, logdir, name):
+def test(model, decoder, test_loader, logdir, name):
     model.eval()
     batch_bar = tqdm(total=len(test_loader), dynamic_ncols=True, position=0, leave=False, desc='Test')
 
     log_result = logdir + '/submission_' + name +'.csv'
     with open(log_result, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=['id', 'predictions'])
-            writer.writeheader()
-            cnt = 0
-            with torch.no_grad():
-                for batch, (x, lx) in enumerate(test_loader):
-                        x = x.to(device)
-                        output, length = model(x, lx)
-
-                        decoded = decode(output, length, beam_width=100)
-                        for s in decoded:
-                            writer.writerow({"id": cnt, "predictions": s})
-                            cnt += 1
+        wr = csv.writer(csvfile)
+        wr.writerow(["id","predictions"])
+        cnt = 0
+        with torch.no_grad():
+            for batch, (x, lx) in enumerate(test_loader):
+                x = x.to(device)
+                output, length = model(x, lx)
+                predictions = make_predictions(output,lx,decoder,PHONEME_MAP)
+                for prediction in predictions :
+                    wr.writerow([cnt,prediction])
+                    cnt += 1
 
     
 if __name__ == '__main__':
@@ -215,18 +209,26 @@ if __name__ == '__main__':
         optimizer = optim.Adam(model.parameters(), lr=ARGS.lr)
         
     criterion = nn.CTCLoss().to(device)
+    #scaler = torch.cuda.amp.GradScaler()
+
+    decoder = CTCBeamDecoder(labels=PHONEME_MAP,log_probs_input=True,beam_width=20)
+    test_decoder = CTCBeamDecoder(labels=PHONEME_MAP,log_probs_input=True,beam_width=100)
 
     BEST_LOSS = 999
-    best_model = model
+    BEST_DIST = np.Inf
+    best_model = None
+    val_interval = 5
     for epoch in range(EPOCHS):
         # You can add validation per-epoch here if you would like
         train_loss, lr_rate = train(epoch, model, train_loader, optimizer, criterion)
-        val_loss = validate(model, val_loader, criterion)
-        if BEST_LOSS >= val_loss:
-            save_checkpoint(val_loss, model, optimizer, epoch, logdir)
-            best_model = model
-            BEST_LOSS = val_loss
-            test(model, test_loader, logdir, str(epoch).zfill(3))
-    
-    test(best_model, test_loader, logdir, 'best')
+        if (epoch+1) % val_interval == 0:
+            val_loss, val_dist = validate(epoch, model, val_loader, criterion, decoder)
+            if BEST_DIST >= val_dist:
+                save_checkpoint(val_loss, model, optimizer, epoch, logdir)
+                best_model = model
+                BEST_LOSS = val_loss
+                BEST_DIST = val_dist
+            #test(model, test_decoder, test_loader, logdir, str(epoch).zfill(3))
+    _, _ = validate(epoch, model, val_loader, criterion, test_decoder)
+    test(best_model, test_decoder, test_loader, logdir, 'best')
 
