c5d744fa4f6fc646482b6bbbc7838b2bb2a30013
sanglee <pksll325@gmail.com>
2022-04-15 14:37:23 +0900

    feat: add embeddings

--- hw3p2/model/biLSTM.py
+++ hw3p2/model/biLSTM.py
@@ -7,24 +7,39 @@ from torchsummaryX import summary
 
 class Network(nn.Module):
 
-    def __init__(self,input_size=13, hidden_size=256, num_layers=3, num_classes=41): 
+    def __init__(self,input_size=13, hidden_size=256, num_layers=4, num_classes=41): 
 
         super(Network, self).__init__()
 
-        # self.embedding = 
+        self.embedding = nn.Sequential(
+            nn.Conv1d(input_size,hidden_size, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm1d(hidden_size),
+            nn.GELU(),
+            nn.Conv1d(hidden_size,hidden_size, kernel_size=5, stride=2, padding=2),
+            nn.BatchNorm1d(hidden_size),
+            nn.GELU(),
+            nn.Dropout(p=0.2),
+            nn.Conv1d(hidden_size,hidden_size, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(hidden_size),
+            nn.Conv1d(hidden_size,hidden_size, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(hidden_size),
+            nn.Dropout(p=0.2),
+            nn.GELU()
+        )
         
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout=0.2, bidirectional=True, batch_first=True)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, dropout=0.2, bidirectional=True, batch_first=True)
 
         linear_hidden_size = hidden_size * 2
         self.classification = nn.Sequential(
                     nn.Linear(linear_hidden_size,2048),
-                    nn.Dropout(0.1),
+                    nn.Dropout(p=0.1),
+                    nn.GELU(),
                     nn.Linear(2048, num_classes)
-                    )
+                )
 
     def forward(self, x, X_lens):
-        
-        packed_input = pack_padded_sequence(x, X_lens, batch_first=True, enforce_sorted=False)
+        emb = self.embedding(x.permute(0,2,1)).permute(0,2,1)
+        packed_input = pack_padded_sequence(emb, X_lens//4, batch_first=True, enforce_sorted=False)
 
         out1, (out2, out3) = self.lstm(packed_input) # TODO: Pass packed input to self.lstm
         out, lengths = pad_packed_sequence(out1)
